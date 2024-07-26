13f5c0b993532a5199588a26b18353c8dd88700d
parin-parikh <parinparikh1@gmail.com>
2024-07-25 23:03:45 -0400

    created document editor section and added title input

--- components/Document.tsx
+++ components/Document.tsx
@@ -1,11 +1,45 @@
 'use client'
 
-function Document({id}: {id: string}) {
+import { FormEvent, useEffect, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
+function Document({ id }: { id: string }) {
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+  const [data, loading, error] = useDocumentData(doc(db, "documents", id));
+
+  useEffect(() => {
+    if(data) {
+        setInput(data.title);
+    }
+  }, [data]);
+
+  const updateTitle = (e: FormEvent) => {
+    e.preventDefault();
+
+    if(input.trim()) {
+        startTransition(async () => {
+            await updateDoc(doc(db, "documents", id), {
+                title: input,
+            });
+        });
+    }
+  };
+
   return (
     <div>
         <div>
-            <form>
+            <form onSubmit={updateTitle}>
                 {/* Update title... */}
+                <Input value={input} onChange={(e) => setInput(e.target.value)} />
+
+                <Button disabled={isUpdating} type="submit">
+                    {isUpdating ? "Updating..." : "Update"}
+                </Button>
 
                 {/* IF */}
                 {/* isOwnder && InviteUser, DeleteDocument */}
